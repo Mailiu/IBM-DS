df17462b6116eaede29502a9402bf8dc8c7283e7
Mailiu <mailiudp@gmail.com>
2025-03-23 12:27:29 -0300

    Lab_curso2

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# - List popular languages for Data Science.
+# - Identify commonly used libraries.
+# - Summarize Data Science tools.
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. NumPy
+# 2. Pandas
+# 3. Scikit-learn
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook   |
+# | TensorFlow         |
+# | PyTorch            |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[3]:
+
+
+# This is a simple arithmetic expression to multiply then add integers.
+result = (3*4) + 5
+print(result)
+
+
+# In[4]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+hours = 200 / 60
+print(hours)
+
+
+# **Objectives:**
+# - List popular languages for Data Science.
+# - Identify commonly used libraries.
+# - Summarize Data Science tools.
+
+# ## Author
+# Mailiu Díaz
